21dedad1c85b009cdebf26ca2a656a01a63b05fb
Yasin Kaya <yasinkaya.121@gmail.com>
2020-09-14 19:43:04 +0300

    added first functional module and test script

---
+++ atcom.py
@@ -0,0 +1,120 @@
+'''
+  Sixfab atcom API 
+  -
+  API for AT communication for Sixfab LTE modules. 
+  -
+  Created by Yasin Kaya (selengalp), September 14, 2020.
+'''
+
+import time
+import serial
+
+# global variables
+TIMEOUT = 3 # seconds
+ser = serial.Serial()
+
+###########################################
+### Private Methods #######################
+###########################################
+
+# Function for printing debug message 
+def debug_print(message):
+	print(message)
+
+# Function for getting time as miliseconds
+def millis():
+	return int(time.time())
+
+# Function for delay as miliseconds
+def delay(ms):
+	time.sleep(float(ms/1000.0))
+
+###############################################
+### ATCom Class ###############################
+###############################################
+
+class ATCom:     
+	timeout = TIMEOUT # default timeout for function and methods on this library.
+	
+	response = "" # variable for modem self.responses
+	compose = "" # variable for command self.composes
+	
+    # Special Characters
+	CTRL_Z = '\x1A'
+	
+	# Initializer function
+	def __init__(self, serial_port="/dev/ttyUSB2", serial_baudrate=115200, module="Quectel EC25", rtscts=False, dsrdtr=False):
+		self.module = module
+		ser.port = serial_port
+		ser.baudrate = serial_baudrate
+		ser.parity=serial.PARITY_NONE
+		ser.stopbits=serial.STOPBITS_ONE
+		ser.bytesize=serial.EIGHTBITS
+		ser.rtscts=rtscts
+		ser.dsrdtr=dsrdtr
+		debug_print(self.module + " initialized!")
+			
+	def __del__(self): 
+		ser.close()
+		ser.__del__()
+		
+ 	# Function for clearing global compose variable 
+	def clear_compose(self):
+		self.compose = ""
+	
+	# Function for getting modem response
+	def get_response(self, desired_response, errors):
+		if (ser.isOpen() == False):
+			ser.open()
+		while 1:	
+			self.response =""
+			while(ser.inWaiting()):
+				self.response += ser.read(ser.inWaiting()).decode('utf-8', errors='ignore')
+			
+			if(self.response.find(desired_response) != -1):
+				#debug_print(self.response)
+				return self.response
+			
+			for err_msg in errors:
+				if(self.response.find(err_msg) != -1):
+					raise RuntimeError("Module responsed with an error message!")
+	
+	# Function for sending at comamand to module
+	def send_at_comm_once(self, command):
+		if (ser.isOpen() == False):
+			ser.open()		
+		self.compose = ""
+		self.compose = str(command) + "\r"
+		ser.reset_input_buffer()
+		ser.write(self.compose.encode())
+		#debug_print(self.compose)
+		
+	# Function for sending at command to BG96_AT.
+	def send_at_comm(self, command, desired_response, err_messages, timeout = None):
+		
+		if timeout is None:
+			timeout = self.timeout
+
+		self.send_at_comm_once(command)
+		
+		self.response =""
+		timer = millis()
+		while 1:
+			if( millis() - timer < timeout): 
+				while(ser.inWaiting()):
+					try: 
+						self.response += ser.read(ser.inWaiting()).decode('utf-8', errors='ignore')
+						delay(100)
+					except Exception as e:
+						debug_print(e.Message)
+
+				if(self.response.find(desired_response) != -1):
+					return self.response
+				
+				else:
+					for x in err_messages:
+						if(self.response.find(x) != -1):
+							raise RuntimeError("Module responsed with error message --> " + str(x))
+			else:
+				raise TimeoutError("timeout!")
+			
---
+++ test.py
@@ -0,0 +1,103 @@
+from atcom import ATCom
+
+at = ATCom()
+
+# 1
+try:
+    response = at.send_at_comm("AT\r\n","OK", ["ERROR", "CME ERROR"])
+except RuntimeError:
+    print("Runtime Error")
+except TimeoutError:
+    print("Timeout Error")
+else:
+    print(response)
+
+# 2
+try:
+    response = at.send_at_comm("ATI\r\n","OK", ["ERROR", "CME ERROR"])
+except RuntimeError:
+    print("Runtime Error")
+except TimeoutError:
+    print("Timeout Error")
+else:
+    print(response)
+
+# 3
+try:
+    response = at.send_at_comm("ATV1\r\n","OK", ["ERROR", "CME ERROR"])
+except RuntimeError:
+    print("Runtime Error")
+except TimeoutError:
+    print("Timeout Error")
+else:
+    print(response)
+
+# 4
+try:
+    response = at.send_at_comm("ATE1\r\n","OK", ["ERROR", "CME ERROR"])
+except RuntimeError:
+    print("Runtime Error")
+except TimeoutError:
+    print("Timeout Error")
+else:
+    print(response)
+
+# 5
+try:
+    response = at.send_at_comm("AT+CMEE=2\r\n","OK", ["ERROR", "CME ERROR"])
+except RuntimeError:
+    print("Runtime Error")
+except TimeoutError:
+    print("Timeout Error")
+else:
+    print(response)
+
+# 6
+try:
+    response = at.send_at_comm("AT+IPR?\r\n","OK", ["ERROR", "CME ERROR"])
+except RuntimeError:
+    print("Runtime Error")
+except TimeoutError:
+    print("Timeout Error")
+else:
+    print(response)
+
+# 7
+try:
+    response = at.send_at_comm("AT+GSN\r\n","OK", ["ERROR", "CME ERROR"])
+except RuntimeError:
+    print("Runtime Error")
+except TimeoutError:
+    print("Timeout Error")
+else:
+    print(response)
+
+# 8
+try:
+    response = at.send_at_comm("AT+QURCCFG=\"urcport\",\"usbat\"\r\n","OK", ["ERROR", "CME ERROR"])
+except RuntimeError:
+    print("Runtime Error")
+except TimeoutError:
+    print("Timeout Error")
+else:
+    print(response)
+
+# 9
+try:
+    response = at.send_at_comm("AT+CPIN?\r\n","OK", ["ERROR", "CME ERROR"])
+except RuntimeError:
+    print("Runtime Error")
+except TimeoutError:
+    print("Timeout Error")
+else:
+    print(response)
+
+# 10
+try:
+    response = at.send_at_comm("AT+QCCID\r\n","OK", ["ERROR", "CME ERROR"])
+except RuntimeError:
+    print("Runtime Error")
+except TimeoutError:
+    print("Timeout Error")
+else:
+    print(response)
