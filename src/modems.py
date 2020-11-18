255721c51364717af8c7ef50306ce61d93e1fa3f
Bugra Isguzar <biisguzar@gmail.com>
2020-11-18 21:38:27 +0300

    added telit modem to modems data

--- src/modems.py
+++ src/modems.py
@@ -1,5 +1,6 @@
 # key: modem name
 # value: a list of interface IDs for AT communication
 modems = {
-    "EG25": ["02", "03"]
+    "EG25": ["02", "03"], # Quectel
+    "LE910": ["05", "06"] # Telit
 }
