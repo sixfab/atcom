e2436c77be8cdda4afecef2ea2371e18615de6f2
Yasin Kaya <yasinkaya.121@gmail.com>
2021-05-28 00:29:43 +0300

    Add EP06 support and fix ME910 issue

--- atcom/modems.py
+++ atcom/modems.py
@@ -19,6 +19,7 @@ ec25 = Modem(vid="2c7c", pid="0125", vendor_name="Quectel", product_name="EC25",
 ec21 = Modem(vid="2c7c", pid="0121", vendor_name="Quectel", product_name="EC21", com_ifs="if02")
 bg96 = Modem(vid="2c7c", pid="0296", vendor_name="Quectel", product_name="BG96", com_ifs="if02")
 bg95 = Modem(vid="2c7c", pid="0700", vendor_name="Quectel", product_name="BG95", com_ifs="if02")
+ep06 = Modem(vid="2c7c", pid="0306", vendor_name="Quectel", product_name="EP06", com_ifs="if02")
 
 # Telit
 le910c1_comp1 = Modem(vid="1bc7", pid="1201", vendor_name="Telit", product_name="LE910C1", com_ifs="if04")
@@ -30,10 +31,11 @@ me910c1_comp2 = Modem(vid="1bc7", pid="1102", vendor_name="Telit", product_name=
 supported_modems = [
     ec25,
     ec21,
-    bg96,
-    bg95,
     le910c1_comp1,
     le910c1_comp2,
     me910c1_comp1,
-    le910c1_comp2,
+    me910c1_comp2,
+    bg96,
+    bg95,
+    ep06,
     ]
