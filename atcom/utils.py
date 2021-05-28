673b93666a7e7e7dd3c228285b2c93cbdf5458b9
Yasin Kaya <yasinkaya.121@gmail.com>
2021-05-29 00:26:46 +0300

    Improve performance by removing usb-devices

--- atcom/utils.py
+++ atcom/utils.py
@@ -1,6 +1,51 @@
 from subprocess import check_output
 from .modems import supported_modems
 
+
+def get_available_ports():
+    ports = check_output("find /sys/bus/usb/devices/usb*/ -name dev", shell=True)
+    ports = ports.decode().split("\n")
+
+    available_ports = []
+
+    for port in ports:
+        if port.endswith("/dev"):
+            port = port[:-4]
+
+        if not port:
+            continue
+
+        try:
+            device_details = check_output("udevadm info -q property --export -p {}".format(port), shell=True)
+        except:
+            continue
+
+        device_details = device_details.decode().split("\n")
+
+        _port_details = {}
+
+        for line in device_details:
+            if line.startswith("DEVNAME="):
+                _port_details["port"] = line[8:].replace("'", "")
+            elif line.startswith("ID_VENDOR="):
+                _port_details["vendor"] = line[10:].replace("'", "")
+            elif line.startswith("ID_VENDOR_ID="):
+                _port_details["vendor_id"] = line[13:].replace("'", "")
+            elif line.startswith("ID_MODEL="):
+                _port_details["model"] = line[9:].replace("'", "")
+            elif line.startswith("ID_MODEL_FROM_DATABASE="):
+                _port_details["model_from_database"] = line[23:].replace("'", "")
+            elif line.startswith("ID_MODEL_ID="):
+                _port_details["product_id"] = line[12:].replace("'", "")
+            elif line.startswith("ID_USB_INTERFACE_NUM="):
+                _port_details["interface"] = "if"+line[21:].replace("'", "")
+
+        if "bus" not in _port_details["port"]:
+            available_ports.append(_port_details)
+
+    return available_ports
+
+
 def find_cellular_modem():
     """function to find supported modem"""
     output = check_output("lsusb", shell=True).decode()
@@ -11,19 +56,6 @@ def find_cellular_modem():
     raise Exception("No supported modem exist!")
 
 
-def find_modem_description(modem):
-    """function to get vendor name and product name on kernel driver"""
-    output = check_output("usb-devices", shell=True).decode().split("\n")
-    for line in output:
-        if line.find(modem.vid) != -1:
-            desc_man_line = output.index(line)+1
-            desc_prod_line = output.index(line)+2
-            modem.desc_vendor = output[desc_man_line].split("=")[1]
-            modem.desc_product = output[desc_prod_line].split("=")[1]
-            return
-    raise Exception("Modem description couldn't find!")
-    
-
 def decide_port():
     """function to decide port name of supported modem"""
     try:
@@ -31,17 +63,14 @@ def decide_port():
     except:
         return (None, None)
     else:
-        try:
-            find_modem_description(modem)
-        except:
-            # Skip descripter control
-            modem.desc_vendor = ""
-            modem.desc_product = ""
-
-        output = check_output("ls -l /dev/serial/by-id", shell=True).decode().split("\n")
-        for line in output:
-            if line.find(modem.com_ifs) != -1 and line.find(modem.desc_vendor) != -1 and line.find(modem.desc_product) != -1 :
-                start = line.find("tty")
-                port_name = "/dev/" + line[start:]
+        ports = get_available_ports()
+        for port in ports:
+            if  modem.com_ifs in port.values() and \
+                modem.vid in port.values() and \
+                modem.pid in port.values():
+                
+                port_name = port.get("port")
+                modem.desc_vendor = port.get("vendor")
+                modem.desc_product = port.get("model", port.get("model_from_database"))
                 return (port_name, modem)
         return (None, None)
