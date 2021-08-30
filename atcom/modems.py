958e5cca8d9aab828140b27bc14bc7d7a61f1e34
Yasin Kaya <yasinkaya.121@gmail.com>
2021-08-30 20:35:56 +0300

    Add support for thales' plsx3 modules

--- atcom/modems.py
+++ atcom/modems.py
@@ -25,10 +25,14 @@ bg95 = Modem(vid="2c7c", pid="0700", vendor_name="Quectel", product_name="BG95",
 ep06 = Modem(vid="2c7c", pid="0306", vendor_name="Quectel", product_name="EP06", com_ifs="if02")
 
 # Telit
-le910c1_comp1 = Modem(vid="1bc7", pid="1201", vendor_name="Telit", product_name="LE910C1", com_ifs="if04")
-le910c1_comp2 = Modem(vid="1bc7", pid="1206", vendor_name="Telit", product_name="LE910C1", com_ifs="if05")
+le910c1_comp1 = Modem(vid="1bc7", pid="1201", vendor_name="Telit", product_name="LE910CX", com_ifs="if04")
+le910c1_comp2 = Modem(vid="1bc7", pid="1206", vendor_name="Telit", product_name="LE910CX", com_ifs="if05")
 me910c1_comp1 = Modem(vid="1bc7", pid="1101", vendor_name="Telit", product_name="ME910C1", com_ifs="if01")
 me910c1_comp2 = Modem(vid="1bc7", pid="1102", vendor_name="Telit", product_name="ME910C1", com_ifs="if01")
+
+# Thales
+plsx3_comp1 = Modem(vid="1e2d", pid="0069", vendor_name="Thales/Cinterion", product_name="PLSx3", com_ifs="if04") # ecm
+plsx3_comp2 = Modem(vid="1e2d", pid="006f", vendor_name="Thales/Cinterion", product_name="PLSx3", com_ifs="if04") # wwan
 
 # List of supported modem
 supported_modems = [
@@ -41,4 +45,6 @@ supported_modems = [
     bg96,
     bg95,
     ep06,
+    plsx3_comp1,
+    plsx3_comp2,
     ]
